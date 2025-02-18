e9a6e5272c276bb80050471e357bfe6d723df935
Lamia <lr357@njit.edu>
2025-02-18 14:17:38 -0500

    fininshing HW

--- M2/Problem1.java
+++ M2/Problem1.java
@@ -1,10 +1,11 @@
 package M2;
 
 public class Problem1 extends BaseClass {
-    private static int[] array1 = {0,1,2,3,4,5,6,7,8,9};   
+    private static int[] array1 = {0,1,2,3,4,5,6,7,8,9};
     private static int[] array2 = {9,8,7,6,5,4,3,2,1,0};
     private static int[] array3 = {0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9};
     private static int[] array4 = {9,9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1,1,0,0}; 
+
     private static void printOdds(int[] arr, int arrayNumber){
         // Only make edits between the designated "Start" and "End" comments
         printArrayInfo(arr, arrayNumber);
@@ -15,14 +16,25 @@ public class Problem1 extends BaseClass {
         // Step 3: Add code to solve the problem (add/commit as needed)
         System.out.print("Output Array: ");
         // Start Solution Edits
-        
+        //lr357 2/18/2025
+        boolean first = true;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) { // Check if the number is odd
+                if (!first) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[i]);
+                first = false;
+            }
+        }
 
         // End Solution Edits
         System.out.println("");
-        System.out.println("______________________________________");
+        System.out.println("__");
     }
+
     public static void main(String[] args) {
-        final String ucid = "mt85"; // <-- change to your UCID
+        final String ucid = "lr357"; // <-- change to your UCID
         // no edits below this line
         printHeader(ucid, 1);
         printOdds(array1,1);
@@ -30,6 +42,5 @@ public class Problem1 extends BaseClass {
         printOdds(array3,3);
         printOdds(array4,4);
         printFooter(ucid, 1);
-        
     }
 }
